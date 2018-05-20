7010e4827d476ccb21f3a6eca6190efeb2ec9bf8
Brad Newman <newmanbradm@gmail.com>
2018-05-20 13:10:19 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
-    if contestant[status]
+    if contestant[:status].downcase == winner
+      return contestant[]
 end
 
 def get_contestant_name(data, occupation)
