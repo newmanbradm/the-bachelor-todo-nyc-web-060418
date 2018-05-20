aac2b51cf0646584849bcad00772f0e08b73e6c4
Brad Newman <newmanbradm@gmail.com>
2018-05-20 13:55:22 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -44,6 +44,7 @@ def get_average_age_for_season(data, season)
   total_age = 0
   total_contestants = 0
   data[season].each do |contestant_hash|
+    total_age += contestant_hash["age"].to_i
     
     
 end
